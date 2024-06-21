e3d2c2202fb3daa7808fa91e933d49c52f5f5b7a
jonk <jonk2412@gmail.com>
2024-06-21 16:16:20 +0300

    work Sol

---
+++ yourSolution.sh
@@ -0,0 +1,8 @@
+chmod u+x generateSecret.sh
+rm -r maliciousFiles/
+mkdir secretDir
+touch secretDir/.secret
+chmod 600 secretDir/.secret
+rm important.link
+./generateSecret.sh
+cat secretDir/.secret
